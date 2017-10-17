e446b6ef0bbc343a0572b240f2771fbfa704186c
Mohit Menghnani <m.menghnani@gmail.com>
2017-10-16 19:36:23 -0700

    Added 3 enemies and the player image but everything is static

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x * 110;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -17,15 +19,16 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function(x,y) {
+   window.ctx.drawImage(Resources.get(this.sprite),(this.x),(this.y));
 };
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy();
-var enemy2 = new Enemy();
-var allEnemies = [enemy1,enemy2];
+var enemy1 = new Enemy(1,60);
+var enemy2 = new Enemy(2,230);
+var enemy3 = new Enemy(3,145);
+var allEnemies = [enemy1,enemy2,enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -38,6 +41,7 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+
 });
 
 //Player class along with player image.
@@ -50,15 +54,30 @@ var Player = function() {
 // a handleInput() method.
 
 Player.prototype.update = function(){
+    window.ctx.drawImage(Resources.get(this.sprite),101,300);
+
 };
 
 Player.prototype.render = function(){
-       // ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
+    window.ctx.drawImage(Resources.get(this.sprite),101,400);
+   // console.log("hello");
 };
 
-Player.prototype.handleInput = function(){
+Player.prototype.handleInput = function(x){
+    if(x == "up"){
+
+    }
+    else if(x == "down"){
+
+
+    }
+    else if(x == "left"){
+
+    }
+    else if(x == "right"){
+
+    }
 };
 
 var player = new Player();
-player.render();
+//player.render();
